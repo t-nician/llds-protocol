69ece7c0b9974307848ba5dd4d1602f11f6b6186
t-nician <dev@senpai.zone>
2024-06-04 18:51:35 -0400

    Tryin to plan out my packet headers and how I'll do ym socket exchanges.

--- src/llds/base.rs
+++ src/llds/base.rs
@@ -1,6 +1,23 @@
+const DATA_SEPARATOR: [u64; 2] = [0x00, 0x00];
+
+
 pub struct Packet {
     id: u8,
     desi: u8,
-    data: [u64],
     version: u8,
+    checksum: u8,
+
+    encoded_headers: [u8; 4], // u8 takes one byte, 4 headers
+    encoded_payload: Vec<u8>, // packet payload ready for sending
+    encoded_packet: Vec<u8>, // header + body data
+}
+
+
+impl Packet {
+    pub fn from_vec(data: Option<Vec<u8>>) -> Result<Packet, &'static str> {
+
+
+        return Err("Something unexpected happened.");
+    }
+
 }
