9b6e7360acd7cefbf80e903bd75c6c5944281259
t-nician <dev@senpai.zone>
2024-06-04 19:47:51 -0400

    So I'm doing things.

--- src/llds/base.rs
+++ src/llds/base.rs
@@ -1,23 +1,49 @@
 const DATA_SEPARATOR: [u64; 2] = [0x00, 0x00];
+const PACKET_VERSION: u8 = 1;
 
 
 pub struct Packet {
     id: u8,
     desi: u8,
     version: u8,
-    checksum: u8,
+    checksum: u16, // fletcher-16
 
-    encoded_headers: [u8; 4], // u8 takes one byte, 4 headers
-    encoded_payload: Vec<u8>, // packet payload ready for sending
-    encoded_packet: Vec<u8>, // header + body data
+    encoded_headers: Vec<u8>, // packet headers.
+    encoded_payload: Vec<u8>, // packet payload.
+    encoded_packet: Vec<u8>, // packet header + payload
 }
 
 
 impl Packet {
-    pub fn from_vec(data: Option<Vec<u8>>) -> Result<Packet, &'static str> {
+    pub fn new() -> Packet {
+        Packet {
+            id: 0,
+            desi: 0,
+            version: PACKET_VERSION,
+            checksum: 0,
 
+            encoded_headers: Vec::new(),
+            encoded_payload: Vec::new(),
+            encoded_packet: Vec::new()
+        }
+    }
+
+    
+    pub fn from_buffer(data: [u8; 4096]) -> Result<Packet, &'static str> {
+        Some(Packet::new());
 
         return Err("Something unexpected happened.");
     }
 
+
+    pub fn from_vector(data: Vec<u8>) -> Result<Packet, &'static str> {
+        Some(Packet::new());
+
+        return Err("Something unexpected happened.");
+    }
+
+
+    pub fn encode(&mut self) {
+
+    }
 }
