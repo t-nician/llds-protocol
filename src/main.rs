18d38ee42e5d5e39ff7e56f7d718156efac369f7
t-nician <dev@senpai.zone>
2024-06-07 12:43:04 -0400

    me love writing code and removing it bc i need to rewrite(my checksum was too smol)

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,6 @@
 //use llds;
 
 fn main() {
-    let mut packet = llds::data::Packet::new(255, 255);
-    let mut buffer = [0u8; 512];
-
-    packet.write_str(&"Hello there");
-    packet.write_packet_to_buffer(&mut buffer);
-
-    packet.println();
-
-    let packet_from_buffer = llds::data::Packet::from_buffer(&buffer);
-
-    packet.println();
+    
     
 }
