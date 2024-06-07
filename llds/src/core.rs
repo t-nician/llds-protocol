16d9f6eac7bc1df9eaabe9f32626f46215686a6f
t-nician <dev@senpai.zone>
2024-06-07 16:40:29 -0400

    omg it works finally.

--- llds/src/core.rs
+++ llds/src/core.rs
@@ -1,33 +1,65 @@
-pub struct Packet {}
-pub struct Channel(u8, Box<dyn Fn(&Packet, &mut Packet) -> ()>);
-
-pub struct Event {
-    on_channel: Vec<Channel>,
-
-    on_packet: Box<dyn Fn(&Packet, &mut Packet) -> ()>,
-    on_transform: Box<dyn Fn(&mut Packet) -> ()>,
+#[derive(Debug)]
+pub struct Packet {
+    pub channel: u8,
+    pub data: Vec<u8>
 }
 
+impl Packet {
+    pub fn new(channel: u8) -> Packet {
+        Packet {
+            channel: channel,
+            data: Vec::new()
+        }
+    }
+}
+
+pub struct Event {
+    pub transformer: Box<dyn Fn(&mut Packet)>,
+    pub connections: Vec<Box<dyn Fn(&Packet)>>,
+    pub channels: Vec<(u8, Box<dyn Fn(&Packet, &mut Packet)>)>
+}
+
+
 impl Event {
-    pub fn new() -> Self {
-        Self {
-            on_packet: Box::from(|_ : &_, _: &mut _|{}),
-            on_channel: Vec::new(),//Box::from(|_ : &_, _: &mut _|{}),
-            on_transform: Box::from(|_: &mut _|{})
+    pub fn new() -> Event {
+        Event {
+            transformer: Box::from(|_: &mut _| {}),
+            connections: Vec::new(),
+            channels: Vec::new()
         }
     }
 
-    pub fn on_packet(&mut self, closure: &'static dyn Fn(&Packet, &mut Packet)) {
-        self.on_packet = Box::from(closure);
+    pub fn add_channel(&mut self, channel: u8, closure: &'static dyn Fn(&Packet, &mut Packet)) {
+        self.channels.push((channel, Box::from(closure)));
+    }   
+
+    pub fn add_listener(&mut self, closure: &'static dyn Fn(&Packet)) {
+        self.connections.push(Box::from(closure));
     }
 
-    pub fn on_channel(&mut self, channel: u8, closure: &'static dyn Fn(&Packet, &mut Packet)) {
-        self.on_channel.push(Channel(channel, Box::from(closure)));
+    pub fn set_transformer(&mut self, closure: &'static dyn Fn(&mut Packet) -> ()) {
+        self.transformer = Box::from(closure);
     }
 
-    pub fn on_transform(&mut self, closure: &'static dyn Fn(&mut Packet)) {
-        self.on_transform = Box::from(closure);
+    pub fn fire_event(&mut self, packet: &mut Packet) -> Packet {
+        let mut response_packet = Packet::new(packet.channel);
+        let mut had_channel = false;
+
+        self.transformer.as_ref()(packet);
+
+        for (channel, listener) in &self.channels {
+            if channel == &packet.channel {
+                listener.as_ref()(packet, &mut response_packet);
+                had_channel = true;
+            }
+        }
+
+        if !had_channel {
+            for listener in &self.connections {
+                listener.as_ref()(packet);
+            }
+        }
+
+        return response_packet;
     }
-
-
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@
 */
 //use llds;
 
+use llds::core::{Event, Packet};
+
 fn main() {
-    let mut event = llds::core::Event::new();
+    /*let mut event = llds::core::Event::new();
 
     event.on_transform(&|packet| {
         // i edit packet before it gets sent to the event!
@@ -16,5 +18,35 @@ fn main() {
 
     event.on_packet(&|received_packet, response_packet|{
         // i take any packets that don't match listening channels!
+    });*/
+
+    let mut event = Event::new();
+
+    event.set_transformer(&|packet|{
+        packet.data.push(69);
     });
+
+    event.add_channel(10, &|received_packet, response_packet| {
+        println!("packet received at channel {:?}", received_packet.data);
+        println!("sending back data");
+
+        for i in 30..50 {
+            response_packet.data.push(i);
+        }
+    });
+
+    event.add_listener(&|packet| {
+        println!("packet had no channel to go too... {:?}", packet.data);
+    });
+
+    for (channel, listener) in &event.channels {
+        println!("{:?}", channel);
+    }
+
+    let mut broadcast_packet = Packet::new(10);
+    let response_packet = event.fire_event(
+        &mut broadcast_packet
+    );
+
+    println!("event response {:?}", response_packet.data);
 }
