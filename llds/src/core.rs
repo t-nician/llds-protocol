8b0859352d05a097b9c6e65766f9af1e4ddab6c9
t-nician <dev@senpai.zone>
2024-06-07 21:07:47 -0400

    we cookin'

--- llds/src/core.rs
+++ llds/src/core.rs
@@ -1,65 +1,44 @@
-#[derive(Debug)]
+pub enum Header {
+    Checksum,
+    Version,
+    Channel,
+    Size,
+    Id
+}
+
 pub struct Packet {
-    pub channel: u8,
-    pub data: Vec<u8>
+    header: Vec<u8>,
+    payload: Vec<u8>,
 }
 
 impl Packet {
-    pub fn new(channel: u8) -> Packet {
+    pub fn new() -> Packet {
         Packet {
-            channel: channel,
-            data: Vec::new()
-        }
-    }
-}
-
-pub struct Event {
-    pub transformer: Box<dyn Fn(&mut Packet)>,
-    pub connections: Vec<Box<dyn Fn(&Packet)>>,
-    pub channels: Vec<(u8, Box<dyn Fn(&Packet, &mut Packet)>)>
-}
-
-
-impl Event {
-    pub fn new() -> Event {
-        Event {
-            transformer: Box::from(|_: &mut _| {}),
-            connections: Vec::new(),
-            channels: Vec::new()
+            header: Vec::new(),
+            payload: Vec::new()
         }
     }
 
-    pub fn add_channel(&mut self, channel: u8, closure: &'static dyn Fn(&Packet, &mut Packet)) {
-        self.channels.push((channel, Box::from(closure)));
-    }   
-
-    pub fn add_listener(&mut self, closure: &'static dyn Fn(&Packet)) {
-        self.connections.push(Box::from(closure));
+    pub fn set_u8_header(&mut self, header: Header, value: &u8) {
+        match header {
+            Header::Version => {}
+            Header::Channel => {},
+            Header::Size => {}
+            _ => {}
+        }
     }
 
-    pub fn set_transformer(&mut self, closure: &'static dyn Fn(&mut Packet) -> ()) {
-        self.transformer = Box::from(closure);
+    pub fn set_u16_header(&mut self, header: Header, value: &u16) {
+        match header {
+            Header::Size => {},
+            _ => {}
+        }
     }
 
-    pub fn fire_event(&mut self, packet: &mut Packet) -> Packet {
-        let mut response_packet = Packet::new(packet.channel);
-        let mut had_channel = false;
-
-        self.transformer.as_ref()(packet);
-
-        for (channel, listener) in &self.channels {
-            if channel == &packet.channel {
-                listener.as_ref()(packet, &mut response_packet);
-                had_channel = true;
-            }
+    pub fn set_u32_header(&mut self, header: Header, value: &u32) {
+        match header {
+            Header::Checksum => {},
+            _ => {}
         }
-
-        if !had_channel {
-            for listener in &self.connections {
-                listener.as_ref()(packet);
-            }
-        }
-
-        return response_packet;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,5 @@
-/*
-    cringe plan btw.
-*/
-//use llds;
-
-use llds::core::{Event, Packet};
+use llds::core::{Packet, Header};
 
 fn main() {
-    /*let mut event = llds::core::Event::new();
-
-    event.on_transform(&|packet| {
-        // i edit packet before it gets sent to the event!
-    });
-
-    event.on_channel(10, &|received_packet, response_packet|{
-        // i take packets with a specific channel number!
-    });
-
-    event.on_packet(&|received_packet, response_packet|{
-        // i take any packets that don't match listening channels!
-    });*/
-
-    let mut event = Event::new();
-
-    event.set_transformer(&|packet|{
-        packet.data.push(69);
-    });
-
-    event.add_channel(10, &|received_packet, response_packet| {
-        println!("packet received at channel {:?}", received_packet.data);
-        println!("sending back data");
-
-        for i in 30..50 {
-            response_packet.data.push(i);
-        }
-    });
-
-    event.add_listener(&|packet| {
-        println!("packet had no channel to go too... {:?}", packet.data);
-    });
-
-    for (channel, listener) in &event.channels {
-        println!("{:?}", channel);
-    }
-
-    let mut broadcast_packet = Packet::new(10);
-    let response_packet = event.fire_event(
-        &mut broadcast_packet
-    );
-
-    println!("event response {:?}", response_packet.data);
+    
 }
