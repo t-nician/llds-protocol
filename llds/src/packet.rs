82a4ad3cbc7b79e1183fb6ecf310acaa8b01371e
t-nician <dev@senpai.zone>
2024-06-09 12:01:57 -0400

    finally, complete packet!

--- llds/src/packet.rs
+++ llds/src/packet.rs
@@ -4,10 +4,10 @@ const SEPARATOR: [u8; 2] = [0, 0];
 const PACKET_VERSION: u8 = 1;
 
 const PACKET_SIZE: u16 = 4096;
-const HEADER_SIZE: u16 = 7;
 const SEPARATOR_SIZE: u16 = SEPARATOR.len() as u16;
 
-const PAYLOAD_SIZE: u16 = PACKET_SIZE - HEADER_SIZE - SEPARATOR_SIZE;
+pub const HEADER_SIZE: u16 = 7;
+pub const PAYLOAD_SIZE: u16 = PACKET_SIZE - HEADER_SIZE - SEPARATOR_SIZE;
 
 pub struct Packet {
     pub id: u8,
@@ -29,7 +29,44 @@ impl Packet {
     pub fn from(buffer: &[u8]) -> Packet {
         let mut packet = Packet::new();
 
+        let checksum = &buffer[1..5];
+
+        let channel = &buffer[5];
+        let id = &buffer[6];
+
+        let payload = &buffer[9..];
+
+        packet.id = id.clone();
+
+        packet.channel = channel.clone();
+        packet.payload = payload.to_vec();
+
+        if packet.generate_checksum().to_be_bytes() != checksum {
+            panic!(
+                "Packet::from(buffer &[u8]) failed checksum!\nGot: {:?}\nGenerated: {:?}",
+                checksum,
+                packet.generate_checksum().to_be_bytes()
+            )
+        }
+
         return packet;
+    }
+
+    pub fn get_packet_size(&self) -> usize {
+        return self.payload.len() + HEADER_SIZE as usize + SEPARATOR_SIZE as usize;
+    }
+
+    pub fn generate_checksum(&self) -> u32 {
+        let mut checksum = fletcher::Fletcher32::new();
+        let mut u32_vector: Vec<u16> = Vec::new();
+
+        for byte in &self.payload {
+            u32_vector.push(byte.clone().into());
+        }
+
+        checksum.update(&u32_vector);
+
+        return checksum.value();
     }
 
     pub fn write_string(&mut self, string: &str) {
@@ -48,26 +85,11 @@ impl Packet {
         let _ = cursor.write(&PACKET_VERSION.to_be_bytes());
         let _ = cursor.write(&self.generate_checksum().to_be_bytes());
 
-        println!("cursor {:?}", cursor);
-
         let _ = cursor.write(&self.channel.to_be_bytes());
         let _ = cursor.write(&self.id.to_be_bytes());
 
         let _ = cursor.write(&SEPARATOR);
 
         let _ = cursor.write(&self.payload);
-    }
-
-    pub fn generate_checksum(&self) -> u32 {
-        let mut checksum = fletcher::Fletcher32::new();
-        let mut u32_vector: Vec<u16> = Vec::new();
-
-        for byte in &self.payload {
-            u32_vector.push(byte.clone().into());
-        }
-
-        checksum.update(&u32_vector);
-
-        return checksum.value();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,5 @@ use llds::event::*;
 use llds::packet::*;
 
 fn main() {
-    let mut packet = Packet::new();
-    let mut buffer: Vec<u8> = Vec::new();
     
-    packet.write_string(&"Hello there!");
-    packet.write_packet_to_buffer(&mut buffer);
-    
-    println!("{:?}", buffer);
 }
