51c6316379cb02a32b585fa1f3fdfdcdec6df840
t-nician <dev@senpai.zone>
2024-06-10 21:05:44 -0400

    now this looks cool.

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,13 @@ use llds::event::*;
 
 
 fn main() {
+    let mut packet = Packet::new(Designation::Get);
+
+    packet.channel = 2;
+    packet.id = 33;
+
+    packet.write_string("Hello world!");
+
     
+
 }
