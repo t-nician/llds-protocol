540fbdba00f8bb0e02b4e3ad3c5afdb00955b912
t-nician <dev@senpai.zone>
2024-06-11 18:17:42 -0400

    more kode

--- src/packet.rs
+++ src/packet.rs
@@ -1,11 +1,26 @@
-use strum_macros::EnumIter;
+use strum_macros::EnumIter; 
+use std::io::Write;
+
+const HEADER_SIZE: usize = 3;
 
 #[derive(Debug, EnumIter, Clone, Copy, PartialEq)]
 pub enum Type {
-    Receiving,
-    Status,
-    Post,
-    Get
+    Receiving = 0,
+    Status = 1,
+    Post = 2,
+    Get = 3
+}
+
+impl Type {
+    pub fn from(u8: u8) -> Self {
+        match u8 {
+            0 => { Type::Receiving }
+            1 => { Type::Status }
+            2 => { Type::Post }
+            3 => { Type::Get }
+            _ => { Type::Receiving }
+        }
+    }
 }
 
 pub struct Packet {
@@ -32,8 +47,31 @@ impl Packet {
     pub fn from(buffer: &[u8]) -> Self {
         let mut packet = Packet::new(Type::Receiving);
 
-        
+        packet.r#type = Type::from(buffer[0]);
+        packet.channel = buffer[1];
+        packet.id = buffer[2];
+
+        packet.payload = buffer[3..].to_vec();
 
         return packet;
+    }
+
+    pub fn get_size(&self) -> usize {
+        return self.payload.len() + HEADER_SIZE
+    }
+
+    pub fn write_payload(&mut self, bytes: &[u8]) {
+        for byte in bytes {
+            self.payload.push(byte.clone());
+        }
+    }
+
+    pub fn write_to_buffer(&self, buffer: &mut [u8]) {
+        let mut cursor = &mut buffer[..];
+
+        cursor.write(&(self.r#type as u8).to_be_bytes()).unwrap();
+        cursor.write(&self.channel.to_be_bytes()).unwrap();
+        cursor.write(&self.id.to_be_bytes()).unwrap();
+        cursor.write(&self.payload).unwrap();
     }
 }
--- tests/packet.rs
+++ tests/packet.rs
@@ -2,18 +2,43 @@ use strum::IntoEnumIterator;
 use llds::packet::{Packet, Type};
 
 #[test]
+fn packet_to_buffer_and_back() {
+    let mut sending_packet = Packet::new(Type::Post);
+    let mut receiving_buffer = Vec::new();
+
+    sending_packet.write_payload("Hello there!".as_bytes());
+
+    receiving_buffer.resize(sending_packet.get_size(), 0);
+    sending_packet.write_to_buffer(&mut receiving_buffer);
+
+    let received_packet = Packet::from(&receiving_buffer);
+
+    assert_eq!(sending_packet.r#type, received_packet.r#type);
+    assert_eq!(sending_packet.channel, received_packet.channel);
+    assert_eq!(sending_packet.id, received_packet.id);
+    assert_eq!(sending_packet.payload, received_packet.payload);
+}
+
+#[test]
 fn packets_made_with_type_are_assigned_type() {
     for packet_type in Type::iter() {
         assert_eq!(
-            Packet::new(
-                packet_type
-            ).r#type, 
-
+            Packet::new(packet_type).r#type, 
             packet_type
         );
     }
 }
 
-
-
-
+#[test]
+fn packet_type_enum_ints_are_aligned() {
+    for packet_type in Type::iter() {
+        assert_eq!(
+            packet_type, 
+            Type::from(packet_type as u8), 
+            "Type ints are not aligned!\nType: {:?}\nNumber: {:?}\nFrom: {:?}",
+            packet_type,
+            packet_type as u8,
+            Type::from(packet_type as u8)
+        )
+    }
+}
